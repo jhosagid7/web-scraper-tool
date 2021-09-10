8b525b34c1fa542a488349ee8e096e130edf91d3
Jhonny Pirela <jhosagid7@gmail.com>
2021-09-09 23:09:13 -0400

    test

--- scraping.py
+++ scraping.py
@@ -4,20 +4,25 @@ from datetime import datetime
 import pandas as pd
 import argparse
 
-"""Scrapes job postings from indeed, optionally by job and location.
-    :param job: job title
-    :param location: Where the job is located
-    :type job: str
-    :type location: str
-    :return: all job postings from all page that match the search results. and save the results in CSV
-    :rtype: requests object
-    :rtype: bs4 object
-    :rtype: pandas object
-    :rtype: argparse object
-    """
+########################################################################################################
+"""
+Scrapes job postings from indeed, optionally by job and location.
+:param job: job title
+:param location: Where the job is located
+:type job: str
+:type location: str
+:return: all job postings from all page that match the search results. and save the results in CSV
+:rtype: requests object
+:rtype: bs4 object
+:rtype: pandas object
+:rtype: argparse object
+"""
+########################################################################################################
 
 def scp_get_url(job=None, location=None):
+    # ----------------------------------------------------------------------------
     """Generate url from job and location"""
+    # ----------------------------------------------------------------------------
     if location and job:
         URL = f"https://www.indeed.com/jobs?q={job}&l={location}"
     elif location and not job:
@@ -31,64 +36,71 @@ def scp_get_url(job=None, location=None):
 
 
 def scp_get_record(card):
-    """Extract job data from a single record
-    :param card: data generated in the function main(job, location)
-    :param title_elem: Job title 
-    :param company_elem: company Name
-    :param location_elem: company Location
-    :param post_date: posted date
-    :param today: Scrape Date
-    :param link_cont_elem: URL to apply to.
-    :param sumary: Required skills
-    :param salary_tag: Annuall sallary.
-    :type card: object
-    :type title_elem: str
-    :type company_elem: str
-    :type location_elem: str
-    :type post_date: str
-    :type today: str
-    :type link_cont_elem: str
-    :type sumary: str
-    :type salary_tag: str"""
+    # ----------------------------------------------------------------------------
+    """
+    Extract job data from a single record
+    :param card:            data generated in the function main(job, location)
+    :param title_elem:      Job title 
+    :param company_elem:    company Name
+    :param location_elem:   company Location
+    :param post_date:       posted date
+    :param today:           Scrape Date
+    :param link_cont_elem:  URL to apply to.
+    :param sumary:          Required skills
+    :param salary_tag:      Annuall sallary.
+    :type card:             list
+    :type title_elem:       str
+    :type company_elem:     str
+    :type location_elem:    str
+    :type post_date:        str
+    :type today:            str
+    :type link_cont_elem:   str
+    :type sumary:           str
+    :type salary_tag:       str
+    """
+    # ----------------------------------------------------------------------------
 
-    title_elem = card.find('div', 'slider_container').find('h2', 'jobTitle')
-    company_elem = card.find('div', 'slider_container').find('span', class_='companyName')
-    location_elem = card.find('div', 'slider_container').find('div', class_='companyLocation')
-    post_date = card.find('div', 'slider_container').find('span', 'date')
-    today = datetime.today().strftime('%Y-%m-%d')  
-    link_cont_elem = card["href"]
-    sumary = card.find('div', 'slider_container').find('div', class_='job-snippet')
-    salary_tag = card.find('div', 'slider_container').find('span', 'salary-snippet')
+    title_elem      = card.find('div', 'slider_container').find('h2', 'jobTitle')
+    company_elem    = card.find('div', 'slider_container').find('span', class_='companyName')
+    location_elem   = card.find('div', 'slider_container').find('div', class_='companyLocation')
+    post_date       = card.find('div', 'slider_container').find('span', 'date')
+    today           = datetime.today().strftime('%Y-%m-%d')  
+    link_cont_elem  = card["href"]
+    sumary          = card.find('div', 'slider_container').find('div', class_='job-snippet')
+    salary_tag      = card.find('div', 'slider_container').find('span', 'salary-snippet')
 
     if salary_tag:
-        salary = salary_tag.attrs.get('aria-label')
+        salary      = salary_tag.attrs.get('aria-label')
     else:
-        salary = ''  
+        salary      = ''  
 
-
+    # ----------------------------------------------------------------------------
     """Assigns the data obtained to the variables to later be saved"""
-    job_title = title_elem.text.strip()
-    company = company_elem.text.strip()
-    job_location = location_elem.text.strip()
-    post_date = post_date.text
-    today = today
-    summary = sumary.get_text('lu')
-    salary = salary
-    job_url = "https://www.indeed.com" + link_cont_elem
+    # ----------------------------------------------------------------------------
+    job_title       = title_elem.text.strip()
+    company         = company_elem.text.strip()
+    job_location    = location_elem.text.strip()
+    post_date       = post_date.text
+    today           = today
+    summary         = sumary.get_text('lu')
+    salary          = salary
+    job_url         = "https://www.indeed.com" + link_cont_elem
         
-    
+    # ----------------------------------------------------------------------------
     """Extract job data from a single record"""
+    # ----------------------------------------------------------------------------
     record = (job_title, company, job_location, post_date, today, summary, salary, job_url)
     
-
+    # ----------------------------------------------------------------------------
     """we print the results of the query to the console"""
-    print("Vacant: " + job_title)
-    print("Company: " + company)
-    print("Place: " + job_location)
-    print("Posted: " + post_date)
-    print("Date: " + today)
-    print("Salary: " + salary)
-    print("Url: " + job_url)
+    # ----------------------------------------------------------------------------
+    print("Vacant: "    + job_title)
+    print("Company: "   + company)
+    print("Place: "     + job_location)
+    print("Posted: "    + post_date)
+    print("Date: "      + today)
+    print("Salary: "    + salary)
+    print("Url: "       + job_url)
     print("Description: " + summary)
     print("*"*140)
     print()
@@ -96,15 +108,17 @@ def scp_get_record(card):
 
 
 def main(job=None, location=None):
+    # ----------------------------------------------------------------------------
     """Run the main program routine"""
+    # ----------------------------------------------------------------------------
     records = []
-    url = scp_get_url(job, location)
+    url     = scp_get_url(job, location)
     
     while True:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        results = soup.find(id="resultsCol")
-        cards = results.find_all('a', class_='tapItem')
+        response    = requests.get(url)
+        soup        = BeautifulSoup(response.text, 'html.parser')
+        results     = soup.find(id="resultsCol")
+        cards       = results.find_all('a', class_='tapItem')
         
         if None in (cards):
             continue
